362bb02e5e28175b17437daac0cf1eb5ee30996f
PR <piotr.rybka87@outlook.com>
2020-02-08 17:05:39 +0100

    added test for operator - some failed

--- test/ast/operator/operator_test.py
+++ test/ast/operator/operator_test.py
@@ -68,3 +68,49 @@ class Test_AstOperator(unittest.TestCase):
         self.assertEqual(tree[2]._identifier, '+')
         self.assertEqual(tree[2]._parameters, 'const A& bagno')
         self.assertEqual(tree[2]._returns, 'A&')
+    
+    def test_OnePublicOperatorWithLongNamespace(self):
+        tree = AbstractTreeBuilder(source_code="""
+        class A{
+            public:
+            A::B::C::D::E::F::G::H::I::J::K::L::M::N::O::P& operator =(const A::B::C::D::E::F::G::H::I::J::K::L::M::N::O::P& x);
+        };
+        """).build_ast()
+        self.assertEqual(len(tree), 2)
+        self.assertEqual(tree[0], ClassExpression('A'))
+        self.assertTrue(isinstance(tree[1], OperatorExpression))
+        self.assertEqual(tree[1]._identifier, '=')
+        self.assertEqual(tree[1]._parameters, 'const A::B::C::D::E::F::G::H::I::J::K::L::M::N::O::P& x')
+        self.assertEqual(tree[1]._returns, 'A::B::C::D::E::F::G::H::I::J::K::L::M::N::O::P&')
+
+    def test_OnePublicOperatorWithGarbageComments(self):
+        tree = AbstractTreeBuilder(source_code="""
+        class A{
+            //Public
+            public:
+            //A& operator =( const A& x );
+            //Komentarz
+            /*
+            Jestes szalona mowie Ci
+            Zawsze nia bylas.
+            Skonczysz wreszcie snic?
+            */
+
+            //Miala matka syna, syna jedynego
+            A& operator + ( const A& bagno); //+ operator
+            //Chciala go wychowac na pana wielkiego
+
+            /*
+                    A& operator * ( const A& bagno);
+            */
+        };
+        """).build_ast()
+
+        self.assertEqual(len(tree), 2)
+        self.assertEqual(tree[0], ClassExpression('A'))
+        self.assertTrue(isinstance(tree[1], OperatorExpression))
+        self.assertEqual(tree[1]._identifier, '+')
+        self.assertEqual(tree[1]._parameters, 'const A& bagno')
+        self.assertEqual(tree[1]._returns, 'A&')
+
+        

0459a155bc2b39b6534572ab553fd7a878013cbf
Sebastian Molerus <SMorelu@ra.rockwell.com>
2020-03-02 08:20:50 +0100

    ut added

--- TreeBuilder/test_ut/test_parsing_utilities.py
+++ TreeBuilder/test_ut/test_parsing_utilities.py
@@ -6,163 +6,179 @@ from TreeBuilder.token_stream import TokenStream
 from mock import Mock
 
 
-def test_get_return_part_as_tokens_not_on_params_begin():
-    token_stream = Mock()
-    token_stream.current_kind.return_value = TokenType.params_end_
+class TestGetReturnParsAsTokensSuite:
+    def __init__(self):
+        pass
 
-    with pytest.raises(Exception):
-        get_return_part_as_tokens(token_stream)
+    def test_not_on_params_begin(self):
+        token_stream = Mock()
+        token_stream.current_kind.return_value = TokenType.params_end_
 
-    assert token_stream.current_kind.call_count == 1
-    assert len(token_stream.mock_calls) == 1
+        with pytest.raises(Exception):
+            get_return_part_as_tokens(token_stream)
 
+        assert token_stream.current_kind.call_count == 1
+        assert len(token_stream.mock_calls) == 1
 
-def test_get_return_part_as_tokens_with_semicolon_as_stop():
-    tr = TokenReader(source_code='''
-    ;const A* Foo(
-    ''')
+    def test_with_semicolon_as_stop(self):
+        tr = TokenReader(source_code='''
+        ;const A* Foo(
+        ''')
 
-    ts = TokenStream(tr)
-    ts.forward()
-    ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-    return_tokens = get_return_part_as_tokens(ts)
+        return_tokens = get_return_part_as_tokens(ts)
 
-    assert len(return_tokens) == 3
-    assert return_tokens[0].content == 'const'
-    assert return_tokens[0].kind == TokenType.const_
+        assert len(return_tokens) == 3
+        assert return_tokens[0].content == 'const'
+        assert return_tokens[0].kind == TokenType.const_
 
-    assert return_tokens[1].content == 'A'
-    assert return_tokens[1].kind == TokenType.identifier_
+        assert return_tokens[1].content == 'A'
+        assert return_tokens[1].kind == TokenType.identifier_
 
-    assert return_tokens[2].content == '*'
-    assert return_tokens[2].kind == TokenType.star_
+        assert return_tokens[2].content == '*'
+        assert return_tokens[2].kind == TokenType.star_
 
+    def test_with_no_stop_token(self):
+        tr = TokenReader(source_code='''
+        Bar& const Foo(
+        ''')
 
-def test_get_return_part_as_tokens_with_no_stop_token():
-    tr = TokenReader(source_code='''
-    Bar& const Foo(
-    ''')
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-    ts = TokenStream(tr)
-    ts.forward()
-    ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
+        return_tokens = get_return_part_as_tokens(ts)
 
-    return_tokens = get_return_part_as_tokens(ts)
+        assert len(return_tokens) == 3
+        assert return_tokens[0].content == 'Bar'
+        assert return_tokens[0].kind == TokenType.identifier_
 
-    assert len(return_tokens) == 3
-    assert return_tokens[0].content == 'Bar'
-    assert return_tokens[0].kind == TokenType.identifier_
+        assert return_tokens[1].content == '&'
+        assert return_tokens[1].kind == TokenType.ref_
 
-    assert return_tokens[1].content == '&'
-    assert return_tokens[1].kind == TokenType.ref_
+        assert return_tokens[2].content == 'const'
+        assert return_tokens[2].kind == TokenType.const_
 
-    assert return_tokens[2].content == 'const'
-    assert return_tokens[2].kind == TokenType.const_
+    def test_no_backward_tokens(self):
+        token_stream = Mock()
+        token_stream.current_kind.return_value = TokenType.params_begin_
+        token_stream.backward.return_value = False
 
+        with pytest.raises(Exception):
+            get_return_part_as_tokens(token_stream)
 
-def test_get_return_part_as_tokens_no_backward_tokens():
-    token_stream = Mock()
-    token_stream.current_kind.return_value = TokenType.params_begin_
-    token_stream.backward.return_value = False
+        assert len(token_stream.mock_calls) == 2
+        assert token_stream.current_kind.call_count == 1
+        assert token_stream.backward.call_count == 1
 
-    with pytest.raises(Exception):
-        get_return_part_as_tokens(token_stream)
+    def test_opening_bracket_as_end_barrier(self):
+        token_stream = Mock()
+        token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.opening_bracket_]
+        token_stream.backward.return_value = True
 
-    assert len(token_stream.mock_calls) == 2
-    assert token_stream.current_kind.call_count == 1
-    assert token_stream.backward.call_count == 1
+        assert len(get_return_part_as_tokens(token_stream)) == 0
 
+        assert len(token_stream.mock_calls) == 4
+        assert token_stream.current_kind.call_count == 2
+        assert token_stream.backward.call_count == 2
 
-def test_get_return_part_as_tokens_opening_bracket_as_end_barrier():
-    token_stream = Mock()
-    token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.opening_bracket_]
-    token_stream.backward.return_value = True
+    def test_closing_bracket_as_end_barrier(self):
+        token_stream = Mock()
+        token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.closing_bracket_]
+        token_stream.backward.return_value = True
 
-    assert len(get_return_part_as_tokens(token_stream)) == 0
+        assert len(get_return_part_as_tokens(token_stream)) == 0
 
-    assert len(token_stream.mock_calls) == 4
-    assert token_stream.current_kind.call_count == 2
-    assert token_stream.backward.call_count == 2
+        assert len(token_stream.mock_calls) == 4
+        assert token_stream.current_kind.call_count == 2
+        assert token_stream.backward.call_count == 2
 
+    def test_semicolon_as_end_barrier(self):
+        token_stream = Mock()
+        token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.semicolon_]
+        token_stream.backward.return_value = True
 
-def test_get_return_part_as_tokens_closing_bracket_as_end_barrier():
-    token_stream = Mock()
-    token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.closing_bracket_]
-    token_stream.backward.return_value = True
+        assert len(get_return_part_as_tokens(token_stream)) == 0
 
-    assert len(get_return_part_as_tokens(token_stream)) == 0
+        assert len(token_stream.mock_calls) == 4
+        assert token_stream.current_kind.call_count == 2
+        assert token_stream.backward.call_count == 2
 
-    assert len(token_stream.mock_calls) == 4
-    assert token_stream.current_kind.call_count == 2
-    assert token_stream.backward.call_count == 2
+    def test_scope_token_is_end_barrier(self):
+        tr = TokenReader(source_code='''
+        public :void Foo(
+        ''')
 
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-def test_get_return_part_as_tokens_semicolon_as_end_barrier():
-    token_stream = Mock()
-    token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.semicolon_]
-    token_stream.backward.return_value = True
+        return_tokens = get_return_part_as_tokens(ts)
 
-    assert len(get_return_part_as_tokens(token_stream)) == 0
+        assert len(return_tokens) == 1
+        assert return_tokens[0].content == 'void'
+        assert return_tokens[0].kind == TokenType.identifier_
 
-    assert len(token_stream.mock_calls) == 4
-    assert token_stream.current_kind.call_count == 2
-    assert token_stream.backward.call_count == 2
+    def test_scope_namespace_is_part_of_return_part(self):
+        tr = TokenReader(source_code='''
+        A::B* const Foo(
+        ''')
 
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-def test_get_return_part_as_tokens_semicolon_as_end_barrier():
-    token_stream = Mock()
-    token_stream.current_kind.side_effect = [TokenType.params_begin_, TokenType.semicolon_]
-    token_stream.backward.return_value = True
+        return_tokens = get_return_part_as_tokens(ts)
 
-    assert len(get_return_part_as_tokens(token_stream)) == 0
+        assert len(return_tokens) == 6
 
-    assert len(token_stream.mock_calls) == 4
-    assert token_stream.current_kind.call_count == 2
-    assert token_stream.backward.call_count == 2
+        assert return_tokens[0].content == 'A'
+        assert return_tokens[1].content == ':'
+        assert return_tokens[2].content == ':'
+        assert return_tokens[3].content == 'B'
+        assert return_tokens[4].content == '*'
+        assert return_tokens[5].content == 'const'
 
+        assert return_tokens[0].kind == TokenType.identifier_
+        assert return_tokens[1].kind == TokenType.colon_
+        assert return_tokens[2].kind == TokenType.colon_
+        assert return_tokens[3].kind == TokenType.identifier_
+        assert return_tokens[4].kind == TokenType.star_
+        assert return_tokens[5].kind == TokenType.const_
 
-def get_return_part_as_tokens_scope_token_is_end_barrier():
-    tr = TokenReader(source_code='''
-    public :void Foo(
-    ''')
+    def test_virtual_not_in_return_part(self):
+        tr = TokenReader(source_code='''
+        virtual A& const Foo(
+        ''')
 
-    ts = TokenStream(tr)
-    ts.forward()
-    ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-    return_tokens = get_return_part_as_tokens(ts)
+        return_tokens = get_return_part_as_tokens(ts)
 
-    assert len(return_tokens) == 1
-    assert return_tokens[0].content == 'void'
-    assert return_tokens[0].kind == TokenType.identifier_
+        assert len(return_tokens) == 3
 
+        assert return_tokens[0].content == 'A'
+        assert return_tokens[1].content == '&'
+        assert return_tokens[2].content == 'const'
 
-def get_return_part_as_tokens_scope_namespace_is_part_of_return_part():
-    tr = TokenReader(source_code='''
-    A::B* const Foo(
-    ''')
+        assert return_tokens[0].kind == TokenType.identifier_
+        assert return_tokens[1].kind == TokenType.ref_
+        assert return_tokens[2].kind == TokenType.const_
 
-    ts = TokenStream(tr)
-    ts.forward()
-    ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
+    def test_position_is_not_changed(self):
+        tr = TokenReader(source_code='''
+            private: virtual A::B::C const* const Bar(
+            ''')
 
-    return_tokens = get_return_part_as_tokens(ts)
+        ts = TokenStream(tr)
+        ts.forward()
+        ts.move_forward_to_token_type(token_type=TokenType.params_begin_)
 
-    assert len(return_tokens) == 6
-
-    assert return_tokens[0].content == 'A'
-    assert return_tokens[1].content == ':'
-    assert return_tokens[2].content == ':'
-    assert return_tokens[3].content == 'B'
-    assert return_tokens[4].content == '*'
-    assert return_tokens[5].content == 'const'
-
-    assert return_tokens[0].kind == TokenType.identifier_
-    assert return_tokens[1].kind == TokenType.colon_
-    assert return_tokens[2].kind == TokenType.colon_
-    assert return_tokens[3].kind == TokenType.identifier_
-    assert return_tokens[4].kind == TokenType.star_
-    assert return_tokens[5].kind == TokenType.const_
-
-
+        saved_token = ts.current_token
+        assert len(get_return_part_as_tokens(ts)) > 0
+        assert saved_token is ts.current_token

0e1536515388dd3848d8b8d98d7ff5df5a9faaab
PRybka <PRybka@ra.rockwell.com>
2020-03-06 13:59:40 +0100

    Removed classfortestsearcher ut, added new enviroment builder, updated filesearcher

---
+++ IncludeParser/ut_tests/create_enviroment_for_test.py
@@ -0,0 +1,139 @@
+import os
+import errno
+import shutil
+
+
+PROJECT_PATH = "\\Env_Test_Path\\Project_Bagno\\"
+INCLUDES_PATH = PROJECT_PATH + "Inc\\"
+INCLUDES_PATH_2 = PROJECT_PATH + "Inc_2\\"
+INCLUDES_PATH_ALL = PROJECT_PATH + "Inc_All\\"
+MODULE_FOR_TEST = PROJECT_PATH + "MODULE_TEST\\MODULE_1\\"
+
+ENV_TEST_PATH = [PROJECT_PATH, INCLUDES_PATH, INCLUDES_PATH_2, INCLUDES_PATH_ALL, MODULE_FOR_TEST]
+
+
+HPP_FILE_LIST_INC = ["Inc_header_1.hpp", "Inc_header_2.hpp", "Inc_header_3.hpp", "Inc_header_4.hpp", "Inc_header_5.hpp"]
+H_FILE_LIST_INC = ["Inc_header_1.h", "Inc_header_2.h", "Inc_header_3.h", "Inc_header_4.h", "Inc_header_5.h"]
+
+HPP_FILE_LIST_INC_2 = ["Inc2_header_1.hpp", "Inc2_header_2.hpp", "Inc2_header_3.hpp", "Inc2_header_4.hpp", "Inc2_header_5.hpp"]
+H_FILE_LIST_INC_2 = ["Inc2_header_1.h", "Inc2_header_2.h", "Inc2_header_3.h", "Inc2_header_4.h", "Inc2_header_5.h"]
+
+SYS_HEADER_LIST = ["#include <iostream>", "#include <stdio.h>", "#include <string>", "#include <algorithm>", "#include <vector>"]
+
+
+HEADER_ALL_FILE_LIST_1 = ["Inc_header_hpp_all.hpp"]
+HEADER_ALL_FILE_LIST_2 = ["Inc2_header_hpp_all.hpp"]
+HEADER_ALL_FILE_LIST_3 = ["Inc_header_h_all.hpp"]
+HEADER_ALL_FILE_LIST_4 = ["Inc2_header_h_all.hpp"]
+
+
+NUMBER_OF_HEADER_FILES = len(HPP_FILE_LIST_INC) + len(H_FILE_LIST_INC) + len(HPP_FILE_LIST_INC_2) + len(H_FILE_LIST_INC_2) + len(HEADER_ALL_FILE_LIST_1) + len(HEADER_ALL_FILE_LIST_2) + len(HEADER_ALL_FILE_LIST_3) + len(HEADER_ALL_FILE_LIST_4)
+
+class EnvTestClass:
+    def __init__(self, test_path):
+        if test_path:
+            self._exp_test_path = []
+            self._test_path = test_path
+        else:
+            print "ERROR"
+    def create_env_for_test(self):
+        self._create_env_folders()
+        self._create_empty_header_files(self._test_path + INCLUDES_PATH, HPP_FILE_LIST_INC)
+        self._create_empty_header_files(self._test_path + INCLUDES_PATH, H_FILE_LIST_INC)
+        self._create_empty_header_files(self._test_path + INCLUDES_PATH_2, HPP_FILE_LIST_INC_2)
+        self._create_empty_header_files(self._test_path + INCLUDES_PATH_2, H_FILE_LIST_INC_2)
+        self._create_filled_headers_for_test(self._test_path + INCLUDES_PATH_ALL, HEADER_ALL_FILE_LIST_1, HPP_FILE_LIST_INC)
+        self._create_filled_headers_for_test(self._test_path + INCLUDES_PATH_ALL, HEADER_ALL_FILE_LIST_2, HPP_FILE_LIST_INC_2)
+        self._create_filled_headers_for_test(self._test_path + INCLUDES_PATH_ALL, HEADER_ALL_FILE_LIST_3, H_FILE_LIST_INC)
+        self._create_filled_headers_for_test(self._test_path + INCLUDES_PATH_ALL, HEADER_ALL_FILE_LIST_4, H_FILE_LIST_INC_2)
+        self._create_Module_1_for_test()
+    
+    def get_exp_test_path(self):
+        return self._exp_test_path
+
+    def _create_env_folders(self):
+        dir_path_str = ""
+        for dir_path in ENV_TEST_PATH:
+            dir_path_str = self._test_path + dir_path
+            if not os.path.exists(dir_path_str):
+                os.makedirs(dir_path_str)
+    
+    def _create_empty_header_files(self, path, file_list):
+        path_str = ""
+        for list_item in file_list:
+            path_str = path + list_item
+            f = open(path_str, "w")
+            f.close()
+            self._exp_test_path.append(path_str)
+    
+    def _create_filled_headers_for_test(self, path, header_all_list, include_to_write_list):
+        path_str = ""
+        for list_item in header_all_list:
+            path_str = path + list_item
+            f = open(path_str, "w")
+            for include_to_write in include_to_write_list:
+                f.write("#include " + "\"" + include_to_write + "\"\n")
+            for sys_include in SYS_HEADER_LIST:
+                f.write(sys_include + "\n")
+            f.close()
+            self._exp_test_path.append(path_str)
+    
+    def _create_Module_1_for_test(self):
+        header_path_str = self._test_path + MODULE_FOR_TEST + "\\module_1.hpp"
+        source_path_str = self._test_path + MODULE_FOR_TEST + "\\module_1.cpp"
+
+        f = open(header_path_str, "w")
+        for header_item in HEADER_ALL_FILE_LIST_1:
+            f.write("#include " + "\"" + header_item + "\"\n")
+
+        for header_item in HEADER_ALL_FILE_LIST_2:
+            f.write("#include " + "\"" + header_item + "\"\n")
+        
+        f.write("\n#define TIME 1\n#define HELLO\nclass Test\n{\n Test();\n};")
+        f.close()
+
+        f = open(source_path_str, "w")
+        f.write("#include \"module_1.hpp\"\n\n//---------------------\n\n")
+
+        for header_item in HEADER_ALL_FILE_LIST_3:
+            f.write("#include           " + "\"" + header_item + "\"\n\n\n\n\n")
+
+        for header_item in HEADER_ALL_FILE_LIST_4:
+            f.write("#include " + "\"" + header_item + "\"\n")
+        f.close()
+
+    def remove_all(self):
+        shutil.rmtree(self._test_path  + "\\Env_Test_Path\\")
+    
+    def is_file_include_path_exists(self, include_path_item):
+        if include_path_item in self._exp_test_path:
+            return True
+        else:
+            return False
+
+        
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print os.path.dirname(os.path.abspath(__file__))
+
+# o = EnvTestClass(os.path.dirname(os.path.abspath(__file__)))
+# o.create_env_for_test()
+# #print o.get_exp_test_path()
+# print "HEADERS---------------------"
+# for item in o.get_exp_test_path():
+#     print item
+
+# print "REMOVE"
+# #o.remove_all()
--- IncludeParser/ut_tests/filesearcher/test_filesearcher.py
+++ IncludeParser/ut_tests/filesearcher/test_filesearcher.py
@@ -1,61 +1,60 @@
 import unittest
+import os
 from IncludeParser.filesearcher import FileSearcher
-from SystemModules.RepoPath.repopath import RepoPath
+from IncludeParser.ut_tests.create_enviroment_for_test import PROJECT_PATH
+from IncludeParser.ut_tests.create_enviroment_for_test import HPP_FILE_LIST_INC
+from IncludeParser.ut_tests.create_enviroment_for_test import EnvTestClass
 
 
-ROOT_DIR_FOR_TEST = RepoPath.get_repository_path()
-
+ROOT_DIR_FOR_TEST = os.path.dirname(os.path.abspath(__file__))
 
 class Test_FileSearcher(unittest.TestCase):
 
     def test_find_path_for_current_include(self):
-        includes_to_find = ['timer_sys.h', 'R_LogOut.hpp', 'ICSVFileReader.h']
-        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST)
-        include_file_searcher_obj.find_file_path(includes_to_find)
-        include_path_found = include_file_searcher_obj.get_found_file_list()
+        test_env_obj = EnvTestClass(ROOT_DIR_FOR_TEST)
+        test_env_obj.create_env_for_test()
+        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST + PROJECT_PATH)
+        include_file_searcher_obj.find_file_path(HPP_FILE_LIST_INC)
+        include_path_found_list = include_file_searcher_obj.get_found_file_list()
 
-        exp_include_found_paths = [
-            ROOT_DIR_FOR_TEST + "\\Lugiks\\EngineR_LogOut.hpp",
-            ROOT_DIR_FOR_TEST + "\\Lugiks\\Engine\\Modules\\ICSVFileReader\\ICSVFileReader.h",
-            ROOT_DIR_FOR_TEST + "\\Lugiks\Engine\\Modules\SystemIncludes\\timer_sys.h"]
+        for include_found_item in include_path_found_list:
+            status = test_env_obj.is_file_include_path_exists(include_found_item)
+            self.assertEqual(status, True)
 
-        self.assertEqual(len(include_path_found), len(exp_include_found_paths))
-        for includeFound in include_path_found:
-            for includeFound in exp_include_found_paths:
-                if includeFound in exp_include_found_paths:
-                    exp_include_found_paths.remove(includeFound)
-        self.assertEqual(len(exp_include_found_paths), 0)
+        test_env_obj.remove_all()
     
     def test_no_includes_to_find(self):
+        test_env_obj = EnvTestClass(ROOT_DIR_FOR_TEST)
+        test_env_obj.create_env_for_test()
         includes_to_find = []
-        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST)
+        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST + PROJECT_PATH)
         include_file_searcher_obj.find_file_path(includes_to_find)
         include_path_found = include_file_searcher_obj.get_found_file_list()
         self.assertEqual(len(include_path_found), 0)
+        test_env_obj.remove_all()
     
     def test_random_includes_that_dont_exist(self):
+        test_env_obj = EnvTestClass(ROOT_DIR_FOR_TEST)
+        test_env_obj.create_env_for_test()
         includes_to_find = ['PanieJakieToBagno.hpp', 'WyszkolePanaWJedenKsiezyc.h', 'YYYYYTakJest.hpp']
-        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST)
+        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST + PROJECT_PATH)
         include_file_searcher_obj.find_file_path(includes_to_find)
         include_path_found = include_file_searcher_obj.get_found_file_list()
         self.assertEqual(len(include_path_found), 0)
-
+        test_env_obj.remove_all()
+    
     def test_mix_includes_that_dont_exist_with_exist(self):
-        includes_to_find = ['sys.hpp', 'R_LogOut.hpp', 'ICSVFileReader.h', 'Sample_Header.hpp', 'PanieJakieToBagno.hpp', 'WyszkolePanaWJedenKsiezyc.h', 'YYYYYTakJest.hpp']
-        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST)
-        include_file_searcher_obj.find_file_path(includes_to_find)
-        include_path_found = include_file_searcher_obj.get_found_file_list()
+        test_env_obj = EnvTestClass(ROOT_DIR_FOR_TEST)
+        test_env_obj.create_env_for_test()
+        mix_list = HPP_FILE_LIST_INC[:]
+        mix_list.append("WyszkolePanaWJedenKsiezyc.h")
+        mix_list.append("YYYYYTakJest.hpp")
+        include_file_searcher_obj = FileSearcher(ROOT_DIR_FOR_TEST + PROJECT_PATH)
+        include_file_searcher_obj.find_file_path(mix_list)
+        include_path_found_list = include_file_searcher_obj.get_found_file_list()
 
-        exp_include_found_paths = [
-            ROOT_DIR_FOR_TEST + "\\Lugiks\\EngineR_LogOut.hpp",
-            ROOT_DIR_FOR_TEST + "\\Lugiks\\Engine\\Modules\\ICSVFileReader\\ICSVFileReader.h",
-            ROOT_DIR_FOR_TEST + "\\Netflix\\Inc\\Sample_Header.hpp",
-            ROOT_DIR_FOR_TEST + "\\Lugiks\\Engine\\Modules\\SystemIncludes\\sys.hpp"]
+        for include_found_item in include_path_found_list:
+            status = test_env_obj.is_file_include_path_exists(include_found_item)
+            self.assertEqual(status, True)
 
-        self.assertEqual(len(include_path_found), len(exp_include_found_paths))
-        for includeFound in include_path_found:
-            for includeFound in exp_include_found_paths:
-                if includeFound in exp_include_found_paths:
-                    exp_include_found_paths.remove(includeFound)
-        self.assertEqual(len(exp_include_found_paths), 0)
-
+        test_env_obj.remove_all()     
